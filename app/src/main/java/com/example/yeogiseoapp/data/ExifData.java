26cf830cce5eb70307c0cb40431258c22f061145
EneloopAA <acidcandy2445@gmail.com>
2019-12-13 22:07:43 +0900

    누락된 파일 추가

---
+++ app/src/main/java/com/example/yeogiseoapp/data/ExifData.java
@@ -0,0 +1,70 @@
+package com.example.yeogiseoapp.data;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ExifData {
+
+    @SerializedName("name")
+    private String name;
+    @SerializedName("latitude")
+    private Float latitude;
+    @SerializedName("longitude")
+    private Float longitude;
+    @SerializedName("time")
+    private long time;
+    @SerializedName("id")
+    private String id;
+    @SerializedName("gid")
+    private String gid;
+
+
+
+
+    public ExifData(){}
+
+    public ExifData(String name, Float longitude, Float latitude, long time, String id, String gid){
+        this.name = name;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.time = time;
+        this.id = id;
+        this.gid = gid;
+    }
+
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setLatitude(Float latitude) {
+        this.latitude = latitude;
+    }
+    public void setLongitude(Float longitude) {
+        this.longitude = longitude;
+    }
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Float getLatitude() {
+        return latitude;
+    }
+    public Float getLongitude() {
+        return longitude;
+    }
+    public long getTime() {
+        return time;
+    }
+
+    public String getGid() {
+        return gid;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
---
+++ app/src/main/java/com/example/yeogiseoapp/data/ImageUploadResponse.java
@@ -0,0 +1,16 @@
+package com.example.yeogiseoapp.data;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ImageUploadResponse {
+
+
+    @SerializedName("name")
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+
+}
